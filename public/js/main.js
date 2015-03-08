ed307868c47034502de7ecdaf414e634921b6a0e
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-03-07 23:30:14 -0500

    Making core js functions work with new navbar/structure. Rename mycss.css to main.css. Deleted contents of main.css. Removed local login form.

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,27 @@
 $(document).ready(function () {
   $.material.init();
 });
+
+var currentuser = $('div.navbar #userinfo').attr("user");
+if (currentuser === "") {
+  currentuser = "None";
+}
+var currentuserid = $('div.navbar #userinfo').attr("uid");
+
+var logouthandler = function(event) {
+  event.preventDefault();
+  $.post('/logout').done(homepage).error(onError);
+};
+
+$("div.navbar li#signout").on("click", logouthandler);
+
+// hide sign out button if no user
+var canyouso = function(username) {
+  if (username === "None") {
+    $('div.navbar li#signout').remove();
+  } else {
+    $('div.navbar li#signin').remove();
+  }
+};
+
+canyouso(currentuser);
